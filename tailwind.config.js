c0e0cd3d9b06d09fff46ab1269337ff1f150f247
bipo38 <bipoben7@gmail.com>
2024-01-02 16:11:51 +0100

    feat: basic config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+      colors: {
+        main: 'hsl(0, 0%, 9%)',
+        gray: 'rgba(255, 255, 255, .05)'
+      },
+    
+      textColor: {
+        main: 'rgb(212 212 212)'
+      },
+
+    extend: {
+    },
   },
   plugins: [],
 }
+
 
