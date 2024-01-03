254b176e32815473a7854c9e6cea5b97879f85f0
bipo38 <bipoben7@gmail.com>
2024-01-03 15:06:37 +0100

    feat: add blog

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-      colors: {
-        main: 'hsl(0, 0%, 9%)',
-        gray: 'rgba(255, 255, 255, .05)'
-      },
-    
-      textColor: {
-        main: 'rgb(212 212 212)'
-      },
-
-    extend: {
+    colors: {
+      main: "hsl(0, 0%, 9%)",
+      gray: "rgba(255, 255, 255, .05)",
+      accent: "hsl(22, 72%, 72%)",
     },
+
+    textColor: {
+      main: "rgb(212 212 212)",
+      invert: "hsl(0, 0%, 9%)",
+    },
+    fontFamily: {
+      sans: ["Nunito Variable", "sans-serif"],
+    },
+
+    extend: {},
   },
   plugins: [],
-}
-
-
+};
