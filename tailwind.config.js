09c6ff50d973408e80d4808b4bc4c6b46cb1ec5d
bipo38 <bipoben7@gmail.com>
2024-01-03 22:57:05 +0100

    feat: add languages colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ export default {
       main: "rgb(212 212 212)",
       invert: "hsl(0, 0%, 9%)",
       accent: "hsl(22, 72%, 72%)",
+      go: "rgb(1, 173, 216)",
+      vue: "#42b883",
+      nuxt: "#00dc82",
+      hono: "rgb(227, 96, 2)",
+      typescript: "#3178c6",
+      scss: "#c69",
     },
     fontFamily: {
       sans: ["Nunito Variable", "sans-serif"],
