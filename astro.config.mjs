f93d89cfd1f1657a4da2a23ea7b865cf99acc337
bipo38 <bipoben7@gmail.com>
2024-01-09 16:05:16 +0000

    feat: external md links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import externalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [
+    tailwind(),
+    mdx({
+      rehypePlugins: [[externalLinks, { target: "_blank" }]],
+    }),
+  ],
   output: "hybrid",
 });
