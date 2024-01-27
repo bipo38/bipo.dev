80bd5acf5fb6ee9f6c0a34146b0715e4a2dc74ce
bipo38 <bipoben7@gmail.com>
2024-01-27 14:15:08 +0100

    revert

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,9 @@
 export const sortByDatePosts = (elements: Array<any>): Array<any> => {
-  return elements.sort((a, b) => {
-    return (
-      new Date(a.data.date).getTime() - new Date(b.data.date).getTime()
-    );
-  })
+  if (!elements) return [];
+
+  return elements
+    .sort((a, b) => {
+      return new Date(a.data.date).getTime() - new Date(b.data.date).getTime();
+    })
     .reverse();
 };
